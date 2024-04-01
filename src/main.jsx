227950e7b2a3354730c0296ee1b1f7ce006d2c49
RicardoCordovilla <ricardocordovillaf@gmail.com>
2024-04-01 01:53:31 -0500

    pwa configs

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 // import 'primereact/resources/themes/md-light-deeppurple/theme.css'
 import 'primereact/resources/themes/lara-light-blue/theme.css'
 import 'primeicons/primeicons.css'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration.js'
+import * as serviceWorkerRegistration from './assets/serviceWorkerRegistration.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
