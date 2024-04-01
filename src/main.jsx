3d43179b71721e1de0081ac0709b0bff0ba1c096
RicardoCordovilla <ricardocordovillaf@gmail.com>
2024-04-01 01:10:39 -0500

    pwa configs

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,24 @@ import App from './App.jsx'
 // import 'primereact/resources/themes/md-light-deeppurple/theme.css'
 import 'primereact/resources/themes/lara-light-blue/theme.css'
 import 'primeicons/primeicons.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+serviceWorkerRegistration.register({
+  onUpdate: async (registration) => {
+    alert('New version available!  Ready to update?')
+    if (registration && registration.waiting) {
+      await registration.unregister()
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' })
+    }
+    window.location.reload()
+  }
+})
+
+
+
